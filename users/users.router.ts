578e86cd3e59a162280906a49d79ec516b1b9ba5
Alexandre Dias <alexbrigido@hotmail.com>
2020-02-23 14:20:22 -0300

    added endpoint users/id get

--- users/users.router.ts
+++ users/users.router.ts
@@ -4,11 +4,23 @@ import {User} from './users.model'
 
 class UsersRouter extends Router {
     applyRooutes(application: restify.Server){
+
         application.get('/users', (req, resp, next)=>{
             User.findAll().then(users=>{
                 resp.json(users)
                 return next()
             })            
+        })
+
+        application.get('users/:id', (req, resp, next)=>{
+            User.findById(req.params.id).then(user=>{
+                if(user){
+                    resp.json(user)
+                    return next()
+                }
+                resp.send(404)
+                return next()
+            })
         })
     }
 }
